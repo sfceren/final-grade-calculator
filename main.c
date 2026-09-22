c7bb63da4596935590176e5ae394312df46a9846
Ceren Benzer <cerenbenzer2004@gmail.com>
2026-09-22 19:46:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -16,11 +16,22 @@ int main() {
     printf("enter your midterm note:");
     scanf("%lf",&midtermnote);
     printf("enter your midterm percent(example:20):");
-    scanf("%lf",&midtermpercent);
+    scanf("%lf", &midtermpercent);
+    if(midtermpercent>=100 || midtermpercent<0) {
+        printf("Error: Midterm percent must be between 0 and 99."); 
+    return 1; }
     printf("enter your target note:");
     scanf("%lf",&targetnote);
     int necessarynote= calculaterequiredfinal( midtermnote,midtermpercent, targetnote);
-printf("Notes of your should be:%d",necessarynote);
+    if (necessarynote > 100) {
+        printf("Target is impossible! You need %d points, but max exam score is 100.", necessarynote);
+    } 
+    else if (necessarynote <= 0) {
+        printf("Congratulations! You have already reached your target without taking the final.");
+    } 
+    else {
+        printf("To achieve your target, you must score at least: %d", necessarynote);
+    }
     return 0;
         
  }
